d3945da84a7812ba8dd625e1cbce94ae949552ea
devdouglasm <devdouglasm@gmail.com>
2025-03-19 05:21:24 -0300

    first commit

---
+++ src/main/java/com/devdouglasm/crud_clients/CrudClientsApplication.java
@@ -0,0 +1,13 @@
+package com.devdouglasm.crud_clients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudClientsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudClientsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devdouglasm/crud_clients/CrudClientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devdouglasm.crud_clients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudClientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
