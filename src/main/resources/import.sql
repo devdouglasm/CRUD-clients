77aee833b577f206ba372b7b0ba9d4e54ccbe182
devdouglasm <devdouglasm@gmail.com>
2025-03-19 17:39:17 -0300

    db properties and db seeding

---
+++ src/main/resources/import.sql
@@ -0,0 +1,16 @@
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('João Silva', '12345678901', 4500.00, '1985-06-15', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Maria Oliveira', '23456789012', 5200.00, '1990-08-20', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Carlos Souza', '34567890123', 3100.00, '1982-03-10', 3;
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Ana Pereira', '45678901234', 6000.00, '1995-12-25', 0));
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Paulo Mendes', '56789012345', 3500.00, '1988-07-05', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Fernanda Lima', '67890123456', 7000.00, '1992-11-30', 1;
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Roberto Costa', '78901234567', 2800.00, '1980-09-18', 4);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Camila Santos', '89012345678', 4700.00, '1997-04-12', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Lucas Rocha', '90123456789', 3200.00, '1984-01-22', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Juliana Alves', '01234567890', 5800.00, '1993-05-08', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Ricardo Nunes', '11234567890', 4100.00, '1987-06-28', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Patrícia Gomes', '21234567890', 5300.00, '1991-10-14', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Gustavo Ribeiro', '31234567890', 2900.00, '1979-12-03', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Vanessa Martins', '41234567890', 6200.00, '1996-08-19', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Fábio Cardoso', '51234567890', 3900.00, '1983-07-07', 2);
+
